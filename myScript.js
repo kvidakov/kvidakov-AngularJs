ce2580393e08dd7d70fdab1810a319cdb408bd7e
kvidakov <kvidakov@foi.hr>
2016-11-29 01:55:23 +0100

    added facebook login

--- myScript.js
+++ myScript.js
@@ -3,139 +3,102 @@
  */
 var app = angular.module("myApp", ["ngRoute"]);
 
-var provider = new firebase.auth.FacebookAuthProvider();
-provider.setCustomParameters({
-    'display': 'popup'
-});
-
-provider.addScope('user_birthday');
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-}).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-});
-
-
-// Step 1.
-// User tries to sign in to Facebook.
-auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).catch(function(error) {
-    // An error happened.
-    if (error.code === 'auth/account-exists-with-different-credential') {
-        // Step 2.
-        // User's email already exists.
-        // The pending Facebook credential.
-        var pendingCred = error.credential;
-        // The provider account's email address.
-        var email = error.email;
-        // Get registered providers for this email.
-        auth.fetchProvidersForEmail(email).then(function(providers) {
-            // Step 3.
-            // If the user has several providers,
-            // the first provider in the list will be the "recommended" provider to use.
-            if (providers[0] === 'password') {
-                // Asks the user his password.
-                // In real scenario, you should handle this asynchronously.
-                var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
-                auth.signInWithEmailAndPassword(email, password).then(function(user) {
-                    // Step 4a.
-                    return user.link(pendingCred);
-                }).then(function() {
-                    // Facebook account successfully linked to the existing Firebase user.
-                    goToApp();
-                });
-                return;
-            }
-            // All the other cases are external providers.
-            // Construct provider object for that provider.
-            // TODO: implement getProviderForProviderId.
-            var provider = getProviderForProviderId(providers[0]);
-            // At this point, you should let the user know that he already has an account
-            // but with a different provider, and let him validate the fact he wants to
-            // sign in with this provider.
-            // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
-            // so in real scenario you should ask the user to click on a "continue" button
-            // that will trigger the signInWithPopup.
-            auth.signInWithPopup(provider).then(function(result) {
-                // Remember that the user may have signed in with an account that has a different email
-                // address than the first one. This can happen as Firebase doesn't control the provider's
-                // sign in flow and the user is free to login using whichever account he owns.
-                // Step 4b.
-                // Link to Facebook credential.
-                // As we have access to the pending credential, we can directly call the link method.
-                result.user.link(pendingCred).then(function() {
-                    // Facebook account successfully linked to the existing Firebase user.
-                    goToApp();
-                });
-            });
-        });
-    }
-});
-
-FB.Event.subscribe('auth.authResponseChange', checkLoginState);
-
-function checkLoginState(event) {
-    if (event.authResponse) {
-        // User is signed-in Facebook.
-        var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-            unsubscribe();
-            // Check if we are already signed-in Firebase with the correct user.
-            if (!isUserEqual(event.authResponse, firebaseUser)) {
-                // Build Firebase credential with the Facebook auth token.
-                var credential = firebase.auth.FacebookAuthProvider.credential(
-                    event.authResponse.accessToken);
-                // Sign in with the credential from the Facebook user.
-                firebase.auth().signInWithCredential(credential).catch(function(error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // The email of the user's account used.
-                    var email = error.email;
-                    // The firebase.auth.AuthCredential type that was used.
-                    var credential = error.credential;
-                    // ...
-                });
+function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+        // [START createprovider]
+        var provider = new firebase.auth.FacebookAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('user_birthday');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-oauthtoken').textContent = token;
+            // [END_EXCLUDE]
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/account-exists-with-different-credential') {
+                alert('You have already signed up with a different auth provider for that email.');
+                // If you are using multiple auth providers on your app you should handle linking
+                // the user's accounts here.
             } else {
-                // User is already signed-in Firebase with the correct user.
+                console.error(error);
             }
+            // [END_EXCLUDE]
         });
+        // [END signin]
     } else {
-        // User is signed-out of Facebook.
+        // [START signout]
         firebase.auth().signOut();
+        // [END signout]
     }
+    // [START_EXCLUDE]
+    document.getElementById('quickstart-sign-in').disabled = true;
+    // [END_EXCLUDE]
 }
-
-function isUserEqual(facebookAuthResponse, firebaseUser) {
-    if (firebaseUser) {
-        var providerData = firebaseUser.providerData;
-        for (var i = 0; i < providerData.length; i++) {
-            if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
-                providerData[i].uid === facebookAuthResponse.userID) {
-                // We don't need to re-auth the Firebase connection.
-                return true;
+// [END buttoncallback]
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    var hasUser = false;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in as: ';
+            document.getElementById('quickstart-sign-in').textContent = 'Log out';
+            document.getElementById('quickstart-account-details').textContent = displayName;
+            var newProfileImgElement = document.createElement("img");
+            newProfileImgElement.setAttribute("src", photoURL);
+            document.getElementById('quickstart-account-details').appendChild(newProfileImgElement);
+            hasUser = true;
+            // [END_EXCLUDE]
+        } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in').textContent = 'Log in with Facebook';
+            if (hasUser)
+            {
+                document.getElementById('quickstart-sign-in-status').textContent = "";
+                document.getElementById('quickstart-account-details').textContent = "";
+                hasUser = false;
             }
+            // [END_EXCLUDE]
         }
-    }
-    return false;
+        // [START_EXCLUDE]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+    });
+    // [END authstatelistener]
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
-
-firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-}, function(error) {
-    // An error happened.
-});
-
+window.onload = function() {
+    initApp();
+};
 
 app.controller("MainController", ["$scope", function ($scope)
 {
